51595bb716e2410f604e68fda12c14dd61d15644
AntonioLuis3181 <xavevel449@ieshnosmachado.org>
2026-05-13 17:02:31 +0200

    Avatar

--- frontend/src/components/AvatarCreator.jsx
+++ frontend/src/components/AvatarCreator.jsx
@@ -1,27 +1,18 @@
-/* eslint-disable react/prop-types */
 import { Suspense, useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Environment, ContactShadows, useGLTF } from '@react-three/drei';
 
-const CUERPO_BASE = '/models/cuerpo_base.glb';
-
 const AvatarEquipado = ({ altura, peso, modeloCamiseta }) => {
-    const { scene: sceneCuerpo } = useGLTF(CUERPO_BASE);
-    const { scene: sceneCamiseta } = useGLTF(`/models/${modeloCamiseta}`);
+    const { scene } = useGLTF(`/models/${modeloCamiseta}`);
+    const sceneClone = useMemo(() => scene.clone(), [scene]);
 
-    const cuerpoClone = useMemo(() => sceneCuerpo.clone(), [sceneCuerpo]);
-    const camisetaClone = useMemo(() => sceneCamiseta.clone(), [sceneCamiseta, modeloCamiseta]);
-
-    const hBase = 170;
-    const pBase = 70;
-    const escalaAltura = altura / hBase;
-    const ratioPeso = (peso - pBase) / pBase;
-    const grosorFinal = Math.max(0.6, Math.min(1.8, 1 + (ratioPeso * 0.8)));
+    const escalaAltura = altura / 170;
+    const ratioPeso = (peso - 70) / 70;
+    const grosorFinal = Math.max(0.6, Math.min(1.8, 1 + ratioPeso * 0.8));
 
     return (
         <group position={[0, -2, 0]} scale={[grosorFinal, escalaAltura, grosorFinal]}>
-            <primitive object={cuerpoClone} />
-            <primitive object={camisetaClone} />
+            <primitive object={sceneClone} />
         </group>
     );
 };
@@ -47,7 +38,5 @@ const AvatarCreator = ({ altura, peso, modeloCamiseta }) => {
         </div>
     );
 };
-
-useGLTF.preload(CUERPO_BASE);
 
 export default AvatarCreator;
